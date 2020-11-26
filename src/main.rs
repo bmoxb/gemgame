e61b56ef5c33562b98e80b2e23caf243e9c37001
WiredSound <maxoblack@yahoo.com>
2020-11-26 20:00:37 +0000

    Add proper logging

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,19 @@ use raylib::prelude::*;
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 fn main() {
+    pretty_env_logger::init();
+
     let (mut handle, thread) = raylib::init()
         .title("Potion/Alchemy Roguelike")
         .size(1280, 720)
         .build();
 
+    log::info!("Initialised RayLib and created window");
+
     let mut current_state: Box::<dyn states::State>;
     current_state = Box::new(states::MainMenu::new());
+
+    log::info!("Created initial state - beginning main loop");
 
     while !handle.window_should_close() {
         // Update game logic:
@@ -25,6 +31,7 @@ fn main() {
         // Draw to screen:
 
         let mut draw = handle.begin_drawing(&thread);
+
         draw.clear_background(Color::BLACK);
 
         current_state.draw(&mut draw);
@@ -34,10 +41,12 @@ fn main() {
         // Handle state transition (if necessary):
 
         if let Some(next_state) = potential_state_change {
-            println!("Changing from state '{}' to '{}'", current_state.title(), next_state.title());
+            log::info!("Changing state from '{}' to '{}'", current_state.title(), next_state.title());
             current_state = next_state;
         }
     }
+
+    log::info!("Exited main loop");
 }
 
 fn draw_debug_text(draw: &mut RaylibDrawHandle, col: Color, size: i32) {
