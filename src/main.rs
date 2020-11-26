9ceffe1b1fa45f4892d9a5f532e04c6fb29cb02b
WiredSound <maxoblack@yahoo.com>
2020-11-26 19:44:36 +0000

    Implement screen/state system

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,47 @@
-mod screens;
+mod states;
 mod world;
 mod items;
 mod ui;
 
 use raylib::prelude::*;
 
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
 fn main() {
-    let (mut rl, thread) = raylib::init()
-        .size(640, 480)
-        .title("Hello, World")
+    let (mut handle, thread) = raylib::init()
+        .title("Potion/Alchemy Roguelike")
+        .size(1280, 720)
         .build();
 
-    while !rl.window_should_close() {
-        let mut d = rl.begin_drawing(&thread);
+    let mut current_state: Box::<dyn states::State>;
+    current_state = Box::new(states::MainMenu::new());
 
-        d.clear_background(Color::WHITE);
-        d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+    while !handle.window_should_close() {
+        // Update game logic:
+
+        let delta = handle.get_frame_time();
+        let potential_state_change = current_state.update(&mut handle, delta);
+
+        // Draw to screen:
+
+        let mut draw = handle.begin_drawing(&thread);
+        draw.clear_background(Color::BLACK);
+
+        current_state.draw(&mut draw);
+
+        draw_debug_text(&mut draw, Color::BLUE, 22);
+
+        // Handle state transition (if necessary):
+
+        if let Some(next_state) = potential_state_change {
+            println!("Changing from state '{}' to '{}'", current_state.title(), next_state.title());
+            current_state = next_state;
+        }
     }
+}
+
+fn draw_debug_text(draw: &mut RaylibDrawHandle, col: Color, size: i32) {
+    draw.draw_text(format!("Version: {}", VERSION).as_str(), 0, 0, size, col);
+    draw.draw_text(format!("Frames: {}/sec", draw.get_fps()).as_str(), 0, size, size, col);
+    draw.draw_text(format!("Delta: {:.2}ms", draw.get_frame_time() * 1000.0).as_str(), 0, size * 2, size, col);
 }
---
+++ src/states/mod.rs
@@ -0,0 +1,53 @@
+//! Module containing all code relating to game 'states' (e.g. the main menu
+//! state, the settings state, the gameplay state, etc.)
+
+use raylib::prelude::*;
+
+pub trait State {
+    fn title(&self) -> &'static str;
+    fn update<'a>(&mut self, handle: &mut RaylibHandle, delta: f32) -> Option<Box<dyn State>>;
+    fn draw(&self, draw: &mut RaylibDrawHandle);
+}
+
+pub struct MainMenu {}
+
+impl MainMenu {
+    pub fn new() -> Self {
+        MainMenu {}
+    }
+}
+
+impl State for MainMenu {
+    fn title(&self) -> &'static str { "Main Menu" }
+
+    fn update(&mut self, handle: &mut RaylibHandle, delta: f32) -> Option<Box<dyn State>> {
+        //println!("{:.5}", delta);
+
+        match handle.get_key_pressed() {
+            Some(_) => Some(Box::new(Game::new())),
+            _ => None
+        }
+    }
+
+    fn draw(&self, draw: &mut RaylibDrawHandle) {
+        draw.draw_circle(200, 200, 50.0, Color::PINK);
+    }
+}
+
+struct Game {
+    // game world, entities, etc.
+}
+
+impl Game {
+    fn new() -> Self {
+        Game {}
+    }
+}
+
+impl State for Game {
+    fn title(&self) -> &'static str { "Game" }
+
+    fn update(&mut self, handle: &mut RaylibHandle, delta: f32) -> Option<Box<dyn State>> { None }
+
+    fn draw(&self, draw: &mut RaylibDrawHandle) {}
+}
