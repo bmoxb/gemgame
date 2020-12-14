f3c7f8af612b61d5d383d8c3ba4d56a299058729
WiredSound <maxoblack@yahoo.com>
2020-12-14 21:36:43 +0000

    Have surface generator randomly place plants

--- src/world/maps/generators.rs
+++ src/world/maps/generators.rs
@@ -2,7 +2,9 @@ use noise::Seedable;
 
 use array_macro::array;
 
-use super::{ Coord, Chunk };
+use noise::NoiseFn;
+
+use super::{ Coord, Chunk, TileType, PlantState, CHUNK_WIDTH, CHUNK_HEIGHT };
 
 pub fn by_name(name: &str, seed: u32) -> Option<Box<dyn Generator>> {
     match name {
@@ -17,12 +19,12 @@ pub trait Generator {
 }
 
 pub struct SurfaceGenerator {
-    noise_gen: noise::Perlin
+    noise_gen: noise::Value
 }
 
 impl SurfaceGenerator {
     pub fn new(seed: u32) -> Self {
-        let noise_gen = noise::Perlin::new();
+        let noise_gen = noise::Value::new();
         noise_gen.set_seed(seed);
 
         SurfaceGenerator { noise_gen }
@@ -33,7 +35,20 @@ impl Generator for SurfaceGenerator {
     fn name(&self) -> &'static str { "surface" }
 
     fn generate(&self, chunk_x: Coord, chunk_y: Coord) -> Chunk {
-        let tiles = array![super::Tile::default(); super::CHUNK_TILE_COUNT];
+        let mut tiles = array![super::Tile::default(); super::CHUNK_TILE_COUNT];
+
+        for offset_x in 0..CHUNK_WIDTH - 1 {
+            for offset_y in 0..CHUNK_HEIGHT - 1 {
+                let tile = &mut tiles[(offset_y * CHUNK_WIDTH + offset_x) as usize];
+
+                let x = (chunk_x * CHUNK_WIDTH + offset_x) as f64;
+                let y = (chunk_y + CHUNK_HEIGHT + offset_y) as f64;
+
+                let value = self.noise_gen.get([x, y, 1.0]);
+                if (value * 100.0) as i32 % 12 == 0 { tile.tile_type = TileType::Flower(PlantState::Ripe); }
+            }
+        }
+
         Chunk::new(tiles) // TODO: Proper chunk generation.
     }
 }
