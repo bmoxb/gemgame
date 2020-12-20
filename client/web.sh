3e8c188769d19dcc15a41a43cf1e471a2b034893
WiredSound <maxoblack@yahoo.com>
2020-12-20 22:18:01 +0000

    Add script for quickly preparing and running a client web build

---
+++ client/web.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+TARGET_DIR="target/wasm32-unknown-unknown/release/"
+readonly TARGET_DIR
+
+set -e -x
+
+cargo build --release --target wasm32-unknown-unknown
+cp web/* $TARGET_DIR
+cd $TARGET_DIR
+python -m http.server
