6ca0af478c180fdd8432a5d007e662106a0b8253
WiredSound <maxoblack@yahoo.com>
2020-12-22 22:55:22 +0000

    Expand networking message enums

--- src/messages.rs
+++ src/messages.rs
@@ -8,11 +8,29 @@ use serde::{ Serialize, Deserialize };
 /// Message sent from the client to the server over the WebSocket protocol.
 #[derive(Debug, Serialize, Deserialize)]
 pub enum ToServer {
-    RequestChunk(maps::Coord, maps::Coord)
+    /// Indicate to the server that this client would like the data for the
+    /// chunk at the specified chunk coordinates. Should the client have a valid
+    /// reason for wanting this chunk (e.g. the client's player character is
+    /// moving towards the requested chunk) then the server will response with
+    /// [`FromServer::ProvideChunk`] with the chunk data.
+    RequestChunk(maps::Coord, maps::Coord),
+
+    /// Inform the server that this client has unloaded a chunk. This is done so
+    /// that the server knows that it does not need to send [`FromServer::UpdateTile`]
+    /// messages for tiles in the specified chunk to this client (the server
+    /// keeps track of what chunks it believes each client has currently loaded).
+    ChunkUnloadedLocally(maps::Coord, maps::Coord)
 }
 
 /// Message sent from the server to the client over the WebSocket protocol.
 #[derive(Serialize, Deserialize)]
 pub enum FromServer {
-    ProvideChunk(maps::Chunk)
+    /// Provide chunk data to a client so it may store it locally. Chunks are
+    /// provided when requested by the client.
+    ProvideChunk(maps::Coord, maps::Coord, maps::Chunk),
+
+    /// Whenever a tile in a chunk is modified, the server sends a message
+    /// about the changed to each client that it believes has the chunk in
+    /// question loaded.
+    UpdateTile(maps::Coord, maps::Coord, maps::Tile)
 }
