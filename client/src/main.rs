3ee4ad54ef1d43af47f1bbef1f74e21cfe462826
WiredSound <maxoblack@yahoo.com>
2020-12-22 22:31:59 +0000

    Add logging for desktop target

--- client/src/main.rs
+++ client/src/main.rs
@@ -10,34 +10,42 @@ const SERVER_PORT: usize = 80;
 
 #[macroquad::main("Client")]
 async fn main() {
+    #[cfg(not(target_arch = "wasm32"))]
+    pretty_env_logger::init(); // Only have logging when build for desktop.
+
+    log::info!("Connecting to '{}' on port {}...", SERVER_ADDRESS, SERVER_PORT);
     let pending = networking::connect(networking::Protocol::WebSocket, SERVER_ADDRESS, SERVER_PORT);
 
-    let mut connecting_text: &str;
-
     loop {
-        let potential_connection = pending.ready();
-
-        match potential_connection {
+        match pending.ready() {
             Err(e) => match e {
-                networking::Error::NotYetConnected => connecting_text = "Connecting...",
-                _ => connecting_text = "Failed to connect to server :("
+                networking::Error::NotYetConnected => {
+                    log::trace!("Connection pending...");
+
+                    quad::draw_text("Connecting...", 0.0, 0.0, 32.0, quad::ORANGE);
+                    quad::next_frame().await;
+                }
+                _ => {
+                    log::warn!("Failed to connect to server!");
+
+                    quad::clear_background(quad::BLACK);
+                    loop {
+                        quad::draw_text("Failed to connect to server :(", 0.0, 0.0, 32.0, quad::RED);
+                        quad::next_frame().await;
+                    }
+                }
             }
 
             Ok(connection) => {
+                log::info!("Connection established! Beginning game loop...");
                 game_loop(connection).await;
                 break;
             }
         }
-
-        quad::clear_background(quad::BLACK);
-        quad::draw_text(connecting_text, 0.0, 0.0, 32.0, quad::ORANGE);
-        quad::next_frame().await;
     }
 }
 
 async fn game_loop(mut connection: impl networking::Connection) {
-    let _ = connection.send(&core::messages::ToServer::RequestChunk(0, 0));
-
     loop {
         quad::clear_background(quad::BLACK);
         quad::draw_text("Connected!", 0.0, 0.0, 32.0, quad::GREEN);
