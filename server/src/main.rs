92bd3561663b6d67ebe47b0faa3b16b5056a69d5
WiredSound <maxoblack@yahoo.com>
2020-12-27 12:40:37 +0000

    Create skeleton server code that uses tokio and tungstenite

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,54 @@
-mod maps;
+//mod maps;
 
-use std::net;
+use std::{
+    net::SocketAddr,
+    sync::{ Arc, Mutex }
+};
+
+use tokio::net::{ TcpListener, TcpStream };
 
 const PORT: usize = 8080;
 
-fn main() {
-    let server = net::TcpListener::bind(format!("127.0.0.1:{}", PORT)).unwrap();
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init_timed();
 
-    for stream in server.incoming() {}
+    let addr = format!("127.0.0.1:{}", PORT);
+
+    let shared = Arc::new(Mutex::new(Shared {}));
+
+    match TcpListener::bind(&addr).await {
+        Ok(listener) => {
+            log::info!("Created TCP/IP listener bound to address: {}", addr);
+
+            while let Ok((stream, addr)) = listener.accept().await {
+                log::info!("Incoming connection from: {}", addr);
+
+                tokio::spawn(handle_connection(shared.clone(), stream, addr));
+            }
+        }
+
+        Err(e) => {
+            log::error!("Failed to create TCP/IP listener due to error: {}", e);
+        }
+    }
+}
+
+/// Data shared between all connection threads.
+struct Shared {}
+
+async fn handle_connection(shared: Arc<Mutex<Shared>>, stream: TcpStream, addr: SocketAddr) {
+    match tokio_tungstenite::accept_async(stream).await {
+        Ok(ws) => {
+            log::debug!("Performed WebSocket handshake successfully with: {}", addr);
+
+            // ...
+
+            log::info!("Client disconnected: {}", addr);
+        }
+
+        Err(e) => {
+            log::error!("Failed to perform WebSocket handshake with '{}' - {}", addr, e);
+        }
+    }
 }
