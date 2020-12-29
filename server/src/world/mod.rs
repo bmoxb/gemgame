a45a14dc66f39150fab1f5337f163d3e01b99196
WiredSound <maxoblack@yahoo.com>
2020-12-29 13:04:59 +0000

    Have World::new create world data directories as necessary

--- server/src/world/mod.rs
+++ server/src/world/mod.rs
@@ -1,27 +1,31 @@
 mod maps;
 
-use std::path::{ PathBuf };
+use std::{ io, path::PathBuf };
 
 pub struct World {
     /// Directory containing world data.
     directory: PathBuf,
 
     /// Currently-loaded maps.
-    loaded_maps: Vec<maps::ServerMap>
+    loaded_maps: Vec<maps::ServerMap>,
+
+    //players: Vec<entities::Entity>
 }
 
+const MAPS_DIRECTORY: &'static str = "maps/";
+const ENTITIES_DIRECTORY: &'static str = "entities/";
+
 impl World {
-    pub fn load_or_new(directory: PathBuf) -> Self {
-        //unimplemented!()
-        World::new(directory)
-    }
+    /// Create a new instance of a game world. Directories for storing world
+    /// data will be created should they not already exist. Any existing world
+    /// data will *not* be deleted.
+    pub async fn new(directory: PathBuf) -> io::Result<Self> {
+        tokio::fs::create_dir_all(directory.join(MAPS_DIRECTORY)).await?;
+        tokio::fs::create_dir_all(directory.join(ENTITIES_DIRECTORY)).await?;
 
-    pub fn load(directory: PathBuf) -> Option<Self> { unimplemented!() }
-
-    pub fn new(directory: PathBuf) -> Self {
-        World {
+        Ok(World {
             directory,
             loaded_maps: Vec::new()
-        }
+        })
     }
 }
