0e0260d5445985bedf4c7f88d3d2b1668304c997
WiredSound <maxoblack@yahoo.com>
2020-12-29 23:40:36 +0000

    Add a 'default' chunk generator for testing purposes

--- server/src/world/maps/generators.rs
+++ server/src/world/maps/generators.rs
@@ -1,11 +1,20 @@
-use core::maps::{ ChunkCoords, Chunk };
+use core::maps::{ ChunkCoords, Chunk, Tile, CHUNK_TILE_COUNT };
 
 pub trait Generator {
     fn generate(&self, coords: ChunkCoords) -> Chunk;
 }
 
+pub struct DefaultGenerator;
+
+impl Generator for DefaultGenerator {
+    fn generate(&self, _: ChunkCoords) -> Chunk {
+        Chunk::new([Tile::default(); CHUNK_TILE_COUNT])
+    }
+}
+
 pub fn by_name(name: &str) -> Option<Box<dyn Generator + Send>> {
     match name {
+        "default" => Some(Box::new(DefaultGenerator)),
         _ => None
     }
 }
