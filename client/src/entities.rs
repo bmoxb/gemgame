9e89725f20a20416617672530121dfc36e20b85f
WiredSound <maxoblack@yahoo.com>
2021-01-10 16:57:32 +0000

    Change doc comment for LocalEntity struct

--- client/src/entities.rs
+++ client/src/entities.rs
@@ -5,7 +5,8 @@ use shared::entities::Entity;
 /// required by both client and server (see [`Entity`]) this structure includes information regarding the rendering and
 /// animating of this entity.
 struct LocalEntity {
-    /// The contained [`Entity`] structure containing entity information not specific to client-side operations.
+    /// The contained [`Entity`] structure contains entity information not specific to just client or server side
+    /// operations.
     contained: Entity,
     /// The position (in camera space) at which the entity will be drawn. This is distinct from the entity's tile/grid
     /// position due to the fact that entities are animated ('slide') between tiles.
