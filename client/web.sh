49b4a58d59fcd126db5f4918fe1895338912c4ec
Max <maxoblack@yahoo.com>
2021-02-20 21:14:49 +0000

    Remove library JavaScript code from repo and fetch it over internet via web build script instead

--- client/web.sh
+++ client/web.sh
@@ -9,4 +9,5 @@ cargo build --release --target wasm32-unknown-unknown
 cp web/* $TARGET_DIR
 cp -r assets/ $TARGET_DIR
 cd $TARGET_DIR
+wget https://not-fl3.github.io/miniquad-samples/gl.js https://not-fl3.github.io/miniquad-samples/sapp_jsutils.js
 python -m http.server
